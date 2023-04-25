4b35025d9a9ef7a6189f99bba5b6cbbaa1853266
Nicole Orbe <nicoleorbe@outlook.com>
2023-04-25 15:37:31 -0500

    moved footer back to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
-import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <Footer />
+      {/* footer */}
+      <footer className="footer">
+        <p>
+          {" "}
+          Built by <span className="span-footer-name">Nicole Orbe</span> and
+          lots of
+          <span className="span-footer-coffee" role="img" aria-label="coffee">
+            ☕
+          </span>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/nicoleorbe/react-weatherapp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weatherapp.thenicoleorbe.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
